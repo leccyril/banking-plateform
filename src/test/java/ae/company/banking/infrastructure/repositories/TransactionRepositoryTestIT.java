cd9953feecc775927ac490d84ab01fd76e7d5388
cyril.leclerc <cyril.leclerc@mantiq.com>
2023-11-29 09:22:09 +0400

    testIT: delete embedded mongo reference.

--- src/test/java/ae/company/banking/infrastructure/repositories/TransactionRepositoryTestIT.java
+++ src/test/java/ae/company/banking/infrastructure/repositories/TransactionRepositoryTestIT.java
@@ -11,11 +11,9 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import reactor.test.StepVerifier;
 
-@DataMongoTest
 @ExtendWith( SpringExtension.class )
 @TestInstance( TestInstance.Lifecycle.PER_CLASS)
 class TransactionRepositoryTestIT {
--- src/test/java/ae/company/banking/infrastructure/repositories/UserRepositoryTestIT.java
+++ src/test/java/ae/company/banking/infrastructure/repositories/UserRepositoryTestIT.java
@@ -5,11 +5,9 @@ import ae.company.banking.domain.user.entities.User;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import reactor.test.StepVerifier;
 
-@DataMongoTest
 @ExtendWith( SpringExtension.class )
 class UserRepositoryTestIT {
 
