2ca0486829e4648841d9645dda0155e5be5911b8
cyril.leclerc <cyril.leclerc@mantiq.com>
2023-11-29 08:29:25 +0400

    ExternalTransfertRepository.java: Change to mono.

--- src/main/java/ae/company/banking/infrastructure/repositories/ExternalTransfertRepository.java
+++ src/main/java/ae/company/banking/infrastructure/repositories/ExternalTransfertRepository.java
@@ -1,11 +1,13 @@
 package ae.company.banking.infrastructure.repositories;
 
-import ae.company.banking.infrastructure.dto.TransferDto;
+import ae.company.banking.infrastructure.dto.ExternalTransferDto;
+import ae.company.banking.infrastructure.dto.InternalTransferDto;
 import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Mono;
 
 @Repository
 public interface ExternalTransfertRepository {
 
 	//Execute the external transfert
-	void executeTransfert(TransferDto dto);
+	Mono<Void> executeTransfert(Mono<ExternalTransferDto> dto);
 }
