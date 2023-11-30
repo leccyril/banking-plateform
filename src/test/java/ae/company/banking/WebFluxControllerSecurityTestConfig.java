2803161351f16c69d6372f0643077e4f5a6b1abf
cyril.leclerc <cyril.leclerc@mantiq.com>
2023-11-30 15:58:32 +0400

    WebFluxControllerSecurityTestConfig.java: Create test configuration.

---
+++ src/test/java/ae/company/banking/WebFluxControllerSecurityTestConfig.java
@@ -0,0 +1,9 @@
+package ae.company.banking;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
+
+@TestConfiguration
+@EnableReactiveMethodSecurity
+public class WebFluxControllerSecurityTestConfig {
+}
