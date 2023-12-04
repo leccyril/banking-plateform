5cf31af030327a1689d08c6f7d42df3fb9a47241
cyril.leclerc <cyril.leclerc@mantiq.com>
2023-12-04 16:26:22 +0400

    BankingApplication.java: Clean code.

--- src/main/java/ae/company/banking/BankingApplication.java
+++ src/main/java/ae/company/banking/BankingApplication.java
@@ -3,9 +3,6 @@ package ae.company.banking;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.data.mongodb.config.EnableMongoAuditing;
-import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
-import org.springframework.web.reactive.config.EnableWebFlux;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
