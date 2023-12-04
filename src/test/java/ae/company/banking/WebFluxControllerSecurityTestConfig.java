a9056fa33ecb9cd4e626585de5d86084153bb213
cyril.leclerc <cyril.leclerc@mantiq.com>
2023-12-04 16:28:27 +0400

    clean code

--- src/test/java/ae/company/banking/WebFluxControllerSecurityTestConfig.java
+++
@@ -1,9 +0,0 @@
-package ae.company.banking;
-
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
-
-@TestConfiguration
-@EnableReactiveMethodSecurity
-public class WebFluxControllerSecurityTestConfig {
-}
